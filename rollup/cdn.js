0ca639e44d8b7e014c9e00e54292bf05eb9082d6
lorenzofox3 <laurent34azerty@gmail.com>
2018-01-23 15:52:58 -0500

    es module for cdns

--- rollup/cdn.js
+++ rollup/cdn.js
@@ -2,11 +2,15 @@ import node from 'rollup-plugin-node-resolve'
 
 export default {
 	input: 'index.js',
-	output: {
+	output: [{
 		file: './dist/smart-table-sort.js',
 		format: 'iife',
 		name: 'smartTableSort',
 		sourcemap: true
-	},
+	}, {
+		file: './dist/smart-table-sort.es.js',
+		format: 'es',
+		sourcemap: true
+	}],
 	plugins: [node()]
 }
