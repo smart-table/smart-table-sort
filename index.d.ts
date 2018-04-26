2cfbb4d929bc3fa9802fa13c88c3c2ee589fbd6f
lorenzofox3 <laurent34azerty@gmail.com>
2018-04-26 12:43:03 +0200

    add type definition

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for smart-table-sort
+
+export as namespace smartTableSort;
+
+export = SmartTableSort;
+
+declare function SmartTableSort(input:SmartTableSort.SortInput): Array<any>;
+
+declare namespace SmartTableSort {
+
+    export const enum SortDirection {
+        ASC = 'asc',
+        DESC = 'desc',
+        NONE = 'none'
+    }
+
+    export interface SortInput {
+        direction?: SortDirection;
+        pointer?: string;
+    }
+}
