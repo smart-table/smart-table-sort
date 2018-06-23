f83fe431958c5447ff1a6032f2fd1ea027334e7e
rhomberg <rhomberg@172.29.61.137>
2018-06-23 14:57:54 +0200

    Added jsdoc, bumped version for new feature

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import {swap} from 'smart-table-operators';
 import pointer from 'smart-table-json-pointer';
 
+/**
+ * Standard comparator that uses default comparison operator
+ *
+ * @param aVal
+ * @param bVal the two values to compare
+ * @return -1 if aVal is to be before after bVal, 1 if bVal is before aVal
+ */
 function standardComparator(aVal, bVal) {
 	if (aVal === bVal) {
 		return 0;
@@ -17,6 +24,11 @@ function standardComparator(aVal, bVal) {
 	return aVal < bVal ? -1 : 1;
 }
 
+/**
+ * Return a compartor that reads the property given by prop to compare two values
+ * @param prop - the path to the property
+ * @param comparator - the comparator to use to compare values
+ */
 function compareByProperty(prop, comparator) {
 	const propGetter = pointer(prop).get;
 	return (a, b) => {
@@ -27,6 +39,12 @@ function compareByProperty(prop, comparator) {
 	};
 }
 
+/**
+ * Create a sort function that sorts an array according to one property
+ * @param pointer - the path to the property used for sorting
+ * @param direction - the direction; 'asc', 'desc', or 'none'
+ * @param comparator - optional comparator function used for sorting
+ */
 export default function sortFactory({pointer, direction, comparator} = {}) {
 	if (!comparator) {
 		comparator = standardComparator;
