c734c16b3709d1055a96ccd7a1e27f8eb7cbc7da
Laurent Renard <laurent@Laurents-MacBook-Pro.local>
2018-10-19 09:59:08 -0400

    better packaging

--- rollup/cdn.js
+++ rollup/cdn.js
@@ -1,16 +1,16 @@
 import node from 'rollup-plugin-node-resolve';
 
 export default {
-  input: './dist/src/index.js',
-  output: [{
-    file: './dist/bundle/smart-table-sort.js',
-    format: 'iife',
-    name: 'smartTableSort',
-    sourcemap: true
-  }, {
-    file: './dist/bundle/smart-table-sort.es.js',
-    format: 'es',
-    sourcemap: true
-  }],
-  plugins: [node()]
-}
+    input: './dist/src/index.js',
+    output: [{
+        file: './dist/bundle/smart-table-sort.js',
+        format: 'iife',
+        name: 'smartTableSort',
+        sourcemap: true
+    }, {
+        file: './dist/bundle/smart-table-sort.es.js',
+        format: 'es',
+        sourcemap: true
+    }],
+    plugins: [node()]
+};
--- test/index.js
+++ test/index.js
@@ -1,76 +1,76 @@
 import test from 'zora';
-import {defaultSortFactory as order} from '../dist/src/index';
+import {defaultSortFactory as order} from '../dist/bundle/module';
 
 test('order list by property value using alpha value', t => {
-  const input = [{prop: 'foo'}, {prop: 'bar'}, {prop: 'woot'}];
-  const output = order({pointer: 'prop'})(input);
-  t.deepEqual(output, [{prop: 'bar'}, {prop: 'foo'}, {prop: 'woot'}]);
+    const input = [{prop: 'foo'}, {prop: 'bar'}, {prop: 'woot'}];
+    const output = order({pointer: 'prop'})(input);
+    t.deepEqual(output, [{prop: 'bar'}, {prop: 'foo'}, {prop: 'woot'}]);
 });
 
 test('order list by property using numeric value', t => {
-  const input = [{prop: 3}, {prop: 1}, {prop: 2}];
-  const output = order({pointer: 'prop'})(input);
-  t.deepEqual(output, [{prop: 1}, {prop: 2}, {prop: 3}]);
+    const input = [{prop: 3}, {prop: 1}, {prop: 2}];
+    const output = order({pointer: 'prop'})(input);
+    t.deepEqual(output, [{prop: 1}, {prop: 2}, {prop: 3}]);
 });
 
 test('order should put undefined value at the end', t => {
-  const input = [{prop: 3}, {foo: 'bar'}, {prop: 2}];
-  const output = order({pointer: 'prop'})(input);
-  t.deepEqual(output, [{prop: 2}, {prop: 3}, {foo: 'bar'}]);
+    const input = [{prop: 3}, {foo: 'bar'}, {prop: 2}];
+    const output = order({pointer: 'prop'})(input);
+    t.deepEqual(output, [{prop: 2}, {prop: 3}, {foo: 'bar'}]);
 });
 
 test('order by reversing direction', t => {
-  const input = [{prop: '3'}, {prop: 1}, {foo: 'bar'}];
-  const output = order({pointer: 'prop', direction: 'desc'})(input);
-  t.deepEqual(output, [{foo: 'bar'}, {prop: '3'}, {prop: 1}]);
+    const input = [{prop: '3'}, {prop: 1}, {foo: 'bar'}];
+    const output = order({pointer: 'prop', direction: 'desc'})(input);
+    t.deepEqual(output, [{foo: 'bar'}, {prop: '3'}, {prop: 1}]);
 });
 
 test('order using nested property', t => {
-  const input = [
-    {foo: {bar: 'bcd'}},
-    {foo: {bar: 'acd'}},
-    {foo: {bar: 'abd'}}
-  ];
-  const output = order({pointer: 'foo.bar'})(input);
-  t.deepEqual(output, [
-    {foo: {bar: 'abd'}},
-    {foo: {bar: 'acd'}},
-    {foo: {bar: 'bcd'}}
-  ]);
+    const input = [
+        {foo: {bar: 'bcd'}},
+        {foo: {bar: 'acd'}},
+        {foo: {bar: 'abd'}}
+    ];
+    const output = order({pointer: 'foo.bar'})(input);
+    t.deepEqual(output, [
+        {foo: {bar: 'abd'}},
+        {foo: {bar: 'acd'}},
+        {foo: {bar: 'bcd'}}
+    ]);
 });
 
-function caseInsensitiveCompare (aVal, bVal) {
-  if (aVal === bVal) {
-    return 0;
-  }
+function caseInsensitiveCompare(aVal, bVal) {
+    if (aVal === bVal) {
+        return 0;
+    }
 
-  if (bVal === undefined) {
-    return -1;
-  }
+    if (bVal === undefined) {
+        return -1;
+    }
 
-  if (aVal === undefined) {
-    return 1;
-  }
+    if (aVal === undefined) {
+        return 1;
+    }
 
-  return aVal.localeCompare(bVal);
+    return aVal.localeCompare(bVal);
 }
 
 test('order list by property value using alpha value with case insensitive compare', t => {
-  const input = [{prop: 'Foo'}, {prop: 'bar'}, {prop: 'woot'}];
-  const output = order({pointer: 'prop', comparator: caseInsensitiveCompare})(input);
-  t.deepEqual(output, [{prop: 'bar'}, {prop: 'Foo'}, {prop: 'woot'}]);
+    const input = [{prop: 'Foo'}, {prop: 'bar'}, {prop: 'woot'}];
+    const output = order({pointer: 'prop', comparator: caseInsensitiveCompare})(input);
+    t.deepEqual(output, [{prop: 'bar'}, {prop: 'Foo'}, {prop: 'woot'}]);
 });
 
 test('order using nested property', t => {
-  const input = [
-    {foo: {bar: 'bcd'}},
-    {foo: {bar: 'Acd'}},
-    {foo: {bar: 'abd'}}
-  ];
-  const output = order({pointer: 'foo.bar', comparator: caseInsensitiveCompare})(input);
-  t.deepEqual(output, [
-    {foo: {bar: 'abd'}},
-    {foo: {bar: 'Acd'}},
-    {foo: {bar: 'bcd'}}
-  ]);
+    const input = [
+        {foo: {bar: 'bcd'}},
+        {foo: {bar: 'Acd'}},
+        {foo: {bar: 'abd'}}
+    ];
+    const output = order({pointer: 'foo.bar', comparator: caseInsensitiveCompare})(input);
+    t.deepEqual(output, [
+        {foo: {bar: 'abd'}},
+        {foo: {bar: 'Acd'}},
+        {foo: {bar: 'bcd'}}
+    ]);
 });
